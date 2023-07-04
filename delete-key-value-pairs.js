b55b30be1d2446c775d3a26860c2270d708817fc
Priyanshu-Tyagi <manutyagi9991@gmail.com>
2023-07-04 16:25:18 +0530

    added code for deleting key value pairs from an JSON data

---
+++ delete-key-value-pairs.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+// Define the path to your JSON file and the keys you want to search for and delete
+const jsonFilePath = 'path/to/your/file.js';
+const keysToDelete = ['key1', 'key2', 'key3'];
+
+// Read the JSON file
+fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading JSON file:', err);
+    return;
+  }
+
+  try {
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+
+    // Search for and delete the specified keys in the JSON data
+    if (Array.isArray(jsonData)) {
+      jsonData.forEach(obj => {
+        keysToDelete.forEach(key => {
+          if (obj.hasOwnProperty(key)) {
+            delete obj[key];
+          }
+        });
+      });
+    }
+
+    // Write the updated JSON data back to the file
+    fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', err => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+        return;
+      }
+
+      console.log(`The specified keys were successfully deleted from the JSON data.`);
+    });
+  } catch (err) {
+    console.error('Error parsing JSON data:', err);
+  }
+});
